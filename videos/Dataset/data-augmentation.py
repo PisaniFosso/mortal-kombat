011fd410d04d66d58f6e7832fc0c9121104d2b02
pisani <epf4168@umoncton.ca>
2019-01-02 09:33:27 -0800

    data augmentation code

--- videos/Dataset/data-augmentation.py
+++ videos/Dataset/data-augmentation.py
@@ -15,6 +15,7 @@ def main():
         draw_single_sequential_images(str(i), "kicks", "kicks-aug")
 
 def draw_single_sequential_images(filename, path, aug_path):
+	print ("loading...")
     image = scipy.misc.imresize(ndimage.imread(path + "/" + filename + ".jpg"), (56, 100))
     sometimes = lambda aug: iaa.Sometimes(0.5, aug)
     seq = iaa.Sequential(
