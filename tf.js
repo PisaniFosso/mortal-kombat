e641e24c872db8dcf6b9e4a0e5cc01cdfaf8f7fc
pisani <epf4168@umoncton.ca>
2019-01-08 15:26:56 -0800

    javascript

---
+++ tf.js
@@ -0,0 +1,11 @@
+import * as tf from '@tensorflow/tfjs';
+
+const model = tf.sequential();
+model.add(tf.layers.inputLayer({ inputShape: [1024] }));
+model.add(tf.layers.dense({ units: 1024, activation: 'relu' }));
+model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
+model.compile({
+  optimizer: tf.train.adam(1e-6),
+  loss: tf.losses.sigmoidCrossEntropy,
+  metrics: ['accuracy']
+});
